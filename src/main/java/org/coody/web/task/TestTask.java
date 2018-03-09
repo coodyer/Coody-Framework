28f1141fe8bb3f70794f33374010c48641752973
644556636@qq.com <644556636@qq.com>
2018-03-10 02:45:53 +0800

    微调

--- src/main/java/org/coody/web/task/TestTask.java
+++ src/main/java/org/coody/web/task/TestTask.java
@@ -2,20 +2,14 @@ package org.coody.web.task;
 
 import org.coody.framework.box.annotation.CronTask;
 import org.coody.framework.box.annotation.InitBean;
-import org.coody.framework.box.iface.InitFace;
 import org.coody.framework.util.DateUtils;
 
 @InitBean
-public class TestTask implements InitFace {
+public class TestTask	 {
 
 	@CronTask("0/5 * * * * ? ")
 	public void test() {
 		System.out.println("定时任务执行中:" + DateUtils.getDateTimeString());
 	}
 
-	@Override
-	public void init() {
-
-		System.out.println("初始化方法执行中");
-	}
 }
