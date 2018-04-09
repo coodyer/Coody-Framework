fb58a69b422ebc8cd1e8f251fbb7e3d7d4c09783
coody <644556636@qq.com>
2018-04-09 12:52:50 +0800

    x

--- src/main/java/org/coody/web/task/TestTask.java
+++ src/main/java/org/coody/web/task/TestTask.java
@@ -7,7 +7,7 @@ import org.coody.framework.util.DateUtils;
 @InitBean
 public class TestTask	 {
 
-	//@CronTask("0/5 * * * * ? ")
+	@CronTask("0/5 * * * * ? ")
 	public void test() {
 		System.out.println("定时任务执行中:" + DateUtils.getDateTimeString());
 	}
