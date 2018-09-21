828bfb4e49aa9d1807d6a22823596850d56641a0
Coody <644556636@qq.com>
2018-09-21 11:03:31 +0800

    优化事物处理

--- coody-jdbc/src/main/java/org/coody/framework/jdbc/aspect/TransactedAspect.java
+++ coody-jdbc/src/main/java/org/coody/framework/jdbc/aspect/TransactedAspect.java
@@ -5,13 +5,17 @@ import java.util.List;
 
 import org.coody.framework.core.annotation.Around;
 import org.coody.framework.core.annotation.AutoBuild;
+import org.coody.framework.core.logger.BaseLogger;
 import org.coody.framework.core.point.AspectPoint;
+import org.coody.framework.core.util.PrintException;
 import org.coody.framework.core.util.StringUtil;
 import org.coody.framework.jdbc.annotation.Transacted;
 import org.coody.framework.jdbc.container.TransactedThreadContainer;
 
 @AutoBuild
 public class TransactedAspect {
+	
+	BaseLogger logger=BaseLogger.getLogger(this.getClass());
 
 	/**
 	 * 事物控制
@@ -29,20 +33,38 @@ public class TransactedAspect {
 			Object result= wrapper.invoke();
 			//提交事物
 			List<Connection> connections=TransactedThreadContainer.getConnections();
-			if(!StringUtil.isNullOrEmpty(connections)){
-				for(Connection conn:connections){
-					try{
-						conn.commit();
-					}catch (Exception e) {
-					}
+			if(StringUtil.isNullOrEmpty(connections)){
+				return result;
+			}
+			for(Connection conn:connections){
+				try{
+					conn.commit();
+				}catch (Exception e) {
+					PrintException.printException(logger, e);
 				}
 			}
 			return result;
-		}finally {
+		}
+		catch (Exception e) {
+			//回滚事物
+			List<Connection> connections=TransactedThreadContainer.getConnections();
+			if(StringUtil.isNullOrEmpty(connections)){
+				throw e;
+			}
+			for(Connection conn:connections){
+				try{
+					conn.rollback();
+				}catch (Exception ex) {
+					PrintException.printException(logger, e);
+				}
+			}
+			throw e;
+		}
+		finally {
 			//关闭连接
 			List<Connection> connections=TransactedThreadContainer.getConnections();
 			TransactedThreadContainer.clear();
-			if(!StringUtil.isNullOrEmpty(connections)){
+			if(StringUtil.isNullOrEmpty(connections)){
 				for(Connection conn:connections){
 					try{
 						conn.close();
