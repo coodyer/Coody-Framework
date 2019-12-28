f7cc52390895b25fdf4bdcf05ff08471cc73ec44
Coody <644556636@qq.com>
2019-12-29 01:40:19 +0800

    修复邮箱smtp重定向问题

--- coody-mail/src/main/java/org/coody/framework/mail/sender/EmailSender.java
+++ coody-mail/src/main/java/org/coody/framework/mail/sender/EmailSender.java
@@ -21,7 +21,7 @@ public class EmailSender {
 
 	static BaseLogger logger = BaseLogger.getLogger(EmailSender.class);
 
-	private EmailSendConfig sendEntity;
+	private EmailSendConfig emailSendConfig;
 
 	private Socket socket;
 
@@ -29,27 +29,35 @@ public class EmailSender {
 
 	private OutputStream outputStream = null;
 
-	public EmailSender(EmailSendConfig sendEntity) {
-		this.sendEntity = sendEntity;
+	public EmailSender(EmailSendConfig emailSendConfig) {
+		this.emailSendConfig = emailSendConfig;
 		init();
 	}
 
 	private synchronized void init() {
 		try {
-			if (sendEntity.getPort() == 25) {
-				socket = new Socket(sendEntity.getSmtp(), sendEntity.getPort());
+			if (emailSendConfig.getPort() == 25) {
+				socket = new Socket(emailSendConfig.getSmtp(), emailSendConfig.getPort());
 			} else {
-				socket = SSLSocketFactory.getDefault().createSocket(sendEntity.getSmtp(), sendEntity.getPort());
+				socket = SSLSocketFactory.getDefault().createSocket(emailSendConfig.getSmtp(),
+						emailSendConfig.getPort());
 			}
 			bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "GBK"));
 			outputStream = socket.getOutputStream();
 
-			readLine();
-			sendAndReadLine("HELO " + sendEntity.getSmtp() + "\r\n");
-			sendAndReadLine("STARTTLS " + sendEntity.getSmtp() + "\r\n");
+			String line = readLine();
+			if (!line.startsWith("220")) {
+				throw new MailException("邮件服务器初始化失败:" + line);
+			}
+			String esmtp = StringUtil.textCutCenter(line, " ", " ");
+			sendAndReadLine("HELO " + esmtp + "\r\n");
 			sendAndReadLine("AUTH LOGIN " + "\r\n");
-			sendAndReadLine(Base64.getEncoder().encodeToString(sendEntity.getEmail().getBytes()) + "\r\n");
-			sendAndReadLine(Base64.getEncoder().encodeToString(sendEntity.getPassword().getBytes()) + "\r\n");
+			sendAndReadLine(Base64.getEncoder().encodeToString(emailSendConfig.getEmail().getBytes()) + "\r\n");
+			line = sendAndReadLine(
+					Base64.getEncoder().encodeToString(emailSendConfig.getPassword().getBytes()) + "\r\n");
+			if (!line.startsWith("235")) {
+				throw new MailException("邮件服务器初始化失败:" + line);
+			}
 		} catch (MailException e) {
 			throw e;
 		} catch (Exception e) {
@@ -58,15 +66,16 @@ public class EmailSender {
 	}
 
 	public void send(List<String> targeEmail, String subject, String content) {
+
 		try {
-			String temp = sendAndReadLine("MAIL FROM:<" + sendEntity.getEmail() + ">" + "\r\n");
+			String temp = sendAndReadLine("MAIL FROM:<" + emailSendConfig.getEmail() + ">" + "\r\n");
 			logger.info(temp);
 			for (String email : targeEmail) {
 				sendAndReadLine("RCPT TO:<" + email + ">" + "\r\n");
 			}
 			sendAndReadLine("DATA " + "\r\n");
 			StringBuilder builder = new StringBuilder();
-			builder.append("From:<" + sendEntity.getEmail() + ">" + "\r\n");
+			builder.append("From:<" + emailSendConfig.getEmail() + ">" + "\r\n");
 
 			for (String email : targeEmail) {
 				builder.append("To:<" + email + ">" + "\r\n");
@@ -78,6 +87,11 @@ public class EmailSender {
 			builder.append(content);
 			builder.append("\r\n" + "." + "\r\n");
 			sendAndReadLine(builder.toString());
+			readLine();
+			readLine();
+			readLine();
+			readLine();
+
 		} catch (MailException e) {
 			throw e;
 		} catch (Exception e) {
@@ -91,7 +105,6 @@ public class EmailSender {
 
 	private String sendAndReadLine(String Command) throws IOException {
 		outputStream.write(Command.getBytes());
-		// flush冲一下
 		outputStream.flush();
 		return readLine();
 	}
@@ -99,6 +112,7 @@ public class EmailSender {
 	private String readLine() throws IOException {
 		String line = bufferedReader.readLine();
 		logger.info(line);
+		System.out.println(line);
 		if (StringUtil.isNullOrEmpty(line)) {
 			throw new MailException("连接邮件服务器失败");
 		}
