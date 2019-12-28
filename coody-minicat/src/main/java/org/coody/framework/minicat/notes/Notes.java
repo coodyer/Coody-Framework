eb2d47a79a00565dac4228825c5524915c2c90af
Coody <644556636@qq.com>
2019-12-28 10:45:33 +0800

    调整日志级别

--- coody-minicat/src/main/java/org/coody/framework/minicat/notes/Notes.java
+++ coody-minicat/src/main/java/org/coody/framework/minicat/notes/Notes.java
@@ -5,18 +5,15 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.coody.framework.core.logger.BaseLogger;
 import org.coody.framework.minicat.threadpool.MiniCatThreadPool;
 
-
 public class Notes {
-	
+
 	static BaseLogger logger = BaseLogger.getLogger(Notes.class);
 
-	public static AtomicLong input=new AtomicLong(0l);
-	
-	public static AtomicLong output=new AtomicLong(0l);
-	
-	
-	
-	static{
+	public static AtomicLong input = new AtomicLong(0l);
+
+	public static AtomicLong output = new AtomicLong(0l);
+
+	static {
 		MiniCatThreadPool.MINICAT_POOL.execute(new Runnable() {
 			public void run() {
 				while (true) {
@@ -25,7 +22,7 @@ public class Notes {
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
-					logger.info("input:"+input+",output:"+output);
+					logger.debug("input:" + input + ",output:" + output);
 					input.set(0);
 					output.set(0);
 				}
