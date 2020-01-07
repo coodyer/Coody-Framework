1ed2489ef7930cece886301b6b2efc08907b7492
Coody <644556636@qq.com>
2020-01-07 16:31:47 +0800

    xxx

--- coody-cson/src/main/java/org/coody/framework/tester/Test7.java
+++ coody-cson/src/main/java/org/coody/framework/tester/Test7.java
@@ -1,6 +1,9 @@
 package org.coody.framework.tester;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,6 +25,7 @@ public class Test7 {
 		Map<Object, Object> map2 = new HashMap<Object, Object>();
 		map2.put("map2_k1", "map2_value1");
 		map2.put(map1, "map2_value2");
+		map2.put(getUserInfo(), "map2_value3");
 		list.add(map2);
 
 		String json = Cson.toJson(list);
@@ -34,5 +38,38 @@ public class Test7 {
 		List<Map<String, Object>> result = wrapper.getObject();
 		System.out.println(Cson.toJson(result));
 
+	}
+
+	private static UserInfo getUserInfo() {
+		UserInfo user = new UserInfo();
+		user.setCreateTime(new Date());
+		user.setDoubleTest(6546.457984654D);
+		user.setFloatTest(1.116578545465f);
+		user.setEmail("644556636@qq.com");
+		user.setId(10086);
+		user.setPassword("123456");
+		user.setStatus(1);
+		user.setResCode(ResCodeEnum.SUCCESS);
+		user.setLocalDateTime(LocalDateTime.now());
+		user.setAttrs(new String[] { "111" });
+		user.setList(Arrays.asList(new String[] { "123", "234" }));
+		user.setCreateTime(new Date());
+		user.setIsAllow(true);
+
+		SettingInfo setting = new SettingInfo();
+		setting.setSiteName("测试标题");
+		setting.setDescription("测试描述");
+		setting.setId(10011);
+		setting.setKeywords("关键词");
+		user.setSetting(setting);
+		Map<Object, Object> map = new HashMap<Object, Object>();
+		map.put("key1", "value1");
+		map.put("key2", "value2");
+		Map<String, Object> map2 = new HashMap<String, Object>();
+		map2.put("map2_k1", "map2_value1");
+		map2.put("map2_k2", "map2_value2");
+		map.put("嵌套MAP", map2);
+		user.setMap(map);
+		return user;
 	}
 }
